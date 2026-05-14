5804889b3f555c17f9aa29f6c5839b615f745af5
Miguel Amiel <m.amiel.gil@gmail.com>
2026-05-15 00:39:34 +0200

    Quitando tests

--- tests-2026/00/printf1.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-main () 
-{
-	printf ("El texto no se imprime %d", 123+1) ;
-}
-
-//@ (main)
-
--- tests-2026/00/puts1.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
- 
-main () 
-{
-	puts (" El texto se imprime ") ;
-}
-
-//@ (main)
-
